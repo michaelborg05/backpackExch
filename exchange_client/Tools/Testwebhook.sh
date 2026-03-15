850a25331de35aafb0f35b8977e2d45f2c7ae238
Michael B <michaelborg05@gmail.com>
2026-03-15 17:12:34 +1100

    stop tracking .sh files

--- exchange_client/Tools/Testwebhook.sh
+++
@@ -1,6 +0,0 @@
-
-curl -H 'Content-Type: application/json' \
-     -d '{"symbol": "SOL_USDC", "action": "buy", "notprice": "23.00", "quantitay": "0.01","secret":"h&ppyfestivu$", "profile":"default"}' \
-     -X POST http://127.0.0.1:8000/webhook/tradingview
-    
-
--- exchange_client/Tools/render_test_webhook.sh
+++
@@ -1,6 +0,0 @@
-
-curl -H 'Content-Type: application/json' \
-     -d '{"symbol": "ETH_USDC", "action": "buy", "notprice": "23.00", "quantity": "0.04","secret":"h&ppyfestivu$", "profile":"15m_MB" }' \
-     -X POST https://borgy-trading.onrender.com/webhook/tradingview
-    
-
